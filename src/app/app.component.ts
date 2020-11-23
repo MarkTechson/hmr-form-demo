dac2a7285502779c88c353923b70384339f607a9
Mark Thompson <markathompson@google.com>
2020-11-23 17:43:18 -0600

    ngmodel demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hmr-form-demo';
+  userName = 'Jeremy';
+  count = 0;
+  users: String[];
+
+  constructor(private userService: UserService) { }
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.users = this.userService.getUsers();
+  }
+
+  addUsers(user: String): void {
+    this.userService.addUser(user);
+  }
 }
