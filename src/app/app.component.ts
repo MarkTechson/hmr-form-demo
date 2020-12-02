3999b765e94fc8909dfb11ff065a94514ba8aeab
Mark Thompson <markathompson@google.com>
2020-12-02 14:14:07 -0600

    updated demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
+import { Applicant } from './applicant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'hmr-form-demo';
-  userName = 'Jeremy';
-  count = 0;
-  users: String[];
+  applicant: Applicant = new Applicant();
 
-  constructor(private userService: UserService) { }
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers(): void {
-    this.users = this.userService.getUsers();
-  }
-
-  addUsers(user: String): void {
-    this.userService.addUser(user);
-  }
+  constructor() { }
 }
